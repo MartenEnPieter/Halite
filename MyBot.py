6d968d14d0f7419f3ed65fc21f354e584b12f653
Pieter <pajkpower@hotmail.com>
2016-12-15 22:27:17 +0100

    More efficient border calculations

--- MyBot.py
+++ MyBot.py
@@ -50,14 +50,76 @@ def move(location):
     
     return Move(location, direction)
 
+def isBorder(location):
+    for d in CARDINALS:
+        neighbour_site = gameMap.getSite(location, d)
+        #Check if all neighbours are of the same owner
+        if neighbour_site.owner != myID:
+            return True
+    
+    return False
+    
+def moveBorder(location):
+    site = gameMap.getSite(location)
+    
+    #Shuffle Cardinals to randomize which direction comes first.
+    LOCALCARDINALS = CARDINALS
+    shuffle(LOCALCARDINALS)
+    direction = STILL
+    minstrength = float("inf")
+
+    for d in LOCALCARDINALS:
+        neighbour_site = gameMap.getSite(location, d)
+        #Check if all neighbours are of the same owner
+        if neighbour_site.owner != myID and neighbour_site.strength < site.strength and neighbour_site.strength < minstrength:
+            #Always take enemy/neutral neighbour if your strenght is enough.
+                minstrength = neighbour_site.strength
+                direction = d
+    
+    return Move(location, direction)
+    
+def moveInterior(location):
+    site = gameMap.getSite(location)
+    direction = STILL
+    
+    if site.strength > 2 * site.production:
+        borderdistance = float("inf")
+        for borderloc in border:
+            if gameMap.getDistance(location, borderloc) < borderdistance:
+                borderdistance = gameMap.getDistance(location, borderloc)
+                angle = gameMap.getAngle(location, borderloc)
+                if angle < math.pi/4 and angle >= -math.pi/4:
+                    direction = EAST
+                if angle < 3*math.pi/4 and angle >= math.pi/4:
+                    direction = NORTH
+                if angle < -3*math.pi/4 or angle >= 3*math.pi/4:
+                    direction = WEST
+                if angle < -math.pi/4 and angle >= -3*math.pi/4:
+                    direction = NORTH
+    
+    return Move(location, direction)
+        
 #Loops as long as the game lasts.
 while True:
+    #f = open('workfile', 'a')
+    #f.write('test')
+    #f.close()
     moves = []
     gameMap = getFrame()
+    interior = []
+    border = []
     #loops over all positions and performs Move function if location is owned by us.
     for y in range(gameMap.height):
         for x in range(gameMap.width):
             location = Location(x, y)
             if gameMap.getSite(location).owner == myID:
-               moves.append(move(location))
+                if isBorder(location):
+                    border.append(location)
+                    moves.append(moveBorder(location))
+                else:
+                    interior.append(location)
+    
+    for location in interior:
+        moves.append(moveInterior(location))
+    
     sendFrame(moves)
